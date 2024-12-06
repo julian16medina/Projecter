067868c2c4fc555c5116f27c21064d4d4e475f79
julian16medina <160174138+julian16medina@users.noreply.github.com>
2024-12-06 15:13:17 -0800

    Initial commit

---
+++ Script.js
@@ -0,0 +1,173 @@
+function changePicture1() {
+  document.getElementById("c1").src = "Images/easyone.jpg";
+  images / jason.png;
+}
+
+let clicking = 0;
+
+let clicks = 0;
+function changePicture2() {
+  document.getElementById("c2").src = "Images/nine.png";
+}
+
+function changePicture3() {
+  document.getElementById("c3").src = "Images/fiver.jpg";
+}
+function remove1() {
+  document.getElementById("first").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    alert("BACK to cell");
+
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove2() {
+  document.getElementById("second").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove3() {
+  document.getElementById("third").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove4() {
+  document.getElementById("fourth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove5() {
+  document.getElementById("monk").play();
+  document.getElementById("fifth").style.display = "none";
+  if (clicking < 2) {
+    clicking = clicking + 1;
+  } else {
+    window.location.href = "https://mcjkm9.csb.app/";
+  }
+}
+
+function remove6() {
+  document.getElementById("sixth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove7() {
+  document.getElementById("seventh").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove8() {
+  document.getElementById("eigth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove9() {
+  document.getElementById("reg").play();
+  document.getElementById("ninth").style.display = "none";
+  if (clicking < 2) {
+    clicking = clicking + 1;
+  } else {
+    window.location.href = "https://mcjkm9.csb.app/";
+  }
+}
+
+function remove10() {
+  document.getElementById("tenth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove11() {
+  document.getElementById("eleventh").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove12() {
+  document.getElementById("twelth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove13() {
+  document.getElementById("eze").play();
+  document.getElementById("thirteenth").style.display = "none";
+  if (clicking < 2) {
+    clicking = clicking + 1;
+  } else {
+    window.location.href = "https://mcjkm9.csb.app/";
+  }
+}
+
+function remove14() {
+  document.getElementById("fourteenth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+
+function remove15() {
+  document.getElementById("fifteenth").style.display = "none";
+  if (clicks < 2) {
+    clicks = clicks + 1;
+  } else {
+    document.getElementById("copie").classList.remove("hidecop");
+    window.location.href = "https://89y3dr.csb.app/";
+  }
+}
+function showModal() {
+  document.getElementById("myModal").style.display = "flex";
+}
+
+function hideModal() {
+  document.getElementById("myModal").style.display = "none";
+}
